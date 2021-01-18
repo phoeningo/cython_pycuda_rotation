b4eca6c73f5feeb3218152f5820d69905d12978e
kkf <33196427+phoeningo@users.noreply.github.com>
2021-01-18 18:42:36 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -5,17 +5,19 @@ from lib import method as M
 #from rotate_core import *
 
 import numpy as np
+#from ftrotate import *
+#from rslice import *
 from pyrotate import *
 from matrix import *
 import pycuda.driver as cuda
-
+from ftslice import *
 import argparse
 parser=argparse.ArgumentParser(description='T')
 parser.add_argument('--input_mrc',type=str,default='test.mrc')
 parser.add_argument('--output_mrc',type=str,default='tmp_rotate.mrc')
 parser.add_argument('--angpix',type=float,default=1.6866)
 parser.add_argument('--rot_seq',type=str,default='zxz')
-parser.add_argument('--angle',type=str,default='90,0,0')
+parser.add_argument('--angle',type=str,default='0,0,0')
 args=parser.parse_args()
 print('done.')
 
@@ -29,15 +31,17 @@ seq=args.rot_seq
 
 input_volume,angpix=M.read_pix_mrc(args.input_mrc)
 output_volume=np.empty_like(input_volume)
-R=np.float32(invMatrix(angle,seq))
+R=np.float32(euler2matrix(angle,seq))
+
+#-----------------------
 
 print('malloc device memory...')
 
-total_mem=cuda.mem_alloc(input_volume.nbytes*2+R.nbytes)
+total_mem=cuda.mem_alloc(input_volume.nbytes+output_volume.nbytes+R.nbytes)
 
 a_gpu=total_mem
 b_gpu=int(a_gpu)+input_volume.nbytes 
-R_gpu=int(b_gpu)+input_volume.nbytes
+R_gpu=int(b_gpu)+output_volume.nbytes
 print('done.')
 
 cuda.memcpy_htod(a_gpu,input_volume)
@@ -45,10 +49,15 @@ cuda.memcpy_htod(R_gpu,R)
 
 
 grid=input_volume.shape
+
 gpu_rotate(np.intp(a_gpu),np.intp(R_gpu),np.intp(b_gpu),grid)
 
 cuda.memcpy_dtoh(output_volume,b_gpu)
-M.write_pix_file(output_volume,args.output_mrc,angpix)
+#M.write_file(np.float32(fo_volume),'test_slice.mrc')
+#output_volume=np.float32(np.abs( np.fft.ifftn ( np.fft.ifftshift(fo_volume))))
+#output_volume=np.float32(np.abs( np.fft.ifftn ( fo_volume)))
+
+M.write_pix_file(zslice(output_volume),args.output_mrc,angpix)
 
 
 
